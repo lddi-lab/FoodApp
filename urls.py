b03eafba485e207dcead8dcc33c6917d6edb3f35
Lucky Icban <lddicban@gmail.com>
2023-06-26 20:55:32 +0800

    Added CRUD on Item

--- urls.py
+++ urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     # food/1
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
+    # add items
+    path('add', views.create_item, name='create_item'),
+    # edit item
+    path('update/<int:id>/', views.update_item, name='update_item'),
+    # delete item
+    path('delete/<int:id>/', views.delete_item, name='delete_item'),
 ]
